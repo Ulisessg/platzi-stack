79acdef1b12f28c40eeeda0cf15a3ec641b75ad0
Ulises SG <ukisessg@gmail.com>
2021-01-23 03:18:35 -0600

    [ADD] Data request responses added

--- network/responses.js
+++ network/responses.js
@@ -1,9 +1,17 @@
-function success(req, res, statusCode = 200, page) {
+function success(req, res, statusCode = 200, message = 'Success') {
+  res.status(statusCode).json(message);
+}
+
+function error(req, res, statusCode = 500, message = 'Internal server error') {
+  res.status(statusCode).json(message);
+}
+
+function page(req, res, statusCode = 200, page) {
   res.status(statusCode).type('html').send(page);
 }
 
-function error(req, res, statusCode = 500, error_page) {
-  res.statusCode(statusCode).type('html').send(error_page);
+function error_page(req, res, statusCode = 200, page) {
+  res.status(statusCode).type('html').send(page);
 }
 
-module.exports = { success, error };
+module.exports = { success, error, page, error_page };
